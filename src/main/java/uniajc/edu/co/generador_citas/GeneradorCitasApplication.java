5ee0ad67a96f5675bd30c4646f4358146ac2d1cc
Jonathan-Palomino-10 <jonathan.palomino.ajc@gmail.com>
2020-09-10 18:20:49 -0700

    Author:Jonathan Palomino @Des:Se quitan import que no se utilizan en las clases

--- src/main/java/uniajc/edu/co/generador_citas/GeneradorCitasApplication.java
+++ src/main/java/uniajc/edu/co/generador_citas/GeneradorCitasApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ApplicationContext;
