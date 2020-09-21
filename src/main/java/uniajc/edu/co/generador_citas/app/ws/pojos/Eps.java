af7596c5e56da957ed0962cc03a17dd635b21ddd
James Arias <james.arias.ajc@gmail.com>
2020-09-21 17:42:11 -0500

    MODIFICACIONES ENTITY IPS

--- src/main/java/uniajc/edu/co/generador_citas/app/ws/pojos/Eps.java
+++ src/main/java/uniajc/edu/co/generador_citas/app/ws/pojos/Eps.java
@@ -3,10 +3,18 @@ package uniajc.edu.co.generador_citas.app.ws.pojos;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@Entity
+@Table(name = "eps")
+@JsonInclude(value = Include.NON_NULL)
 public class Eps implements Serializable {
 
 	private static final long serialVersionUID = 1L;
